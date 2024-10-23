fd974790b030d7e784d5e851ab6a8071405dcd56
nwattamwar25 <nikhilwattamwar25@gmail.com>
2024-10-23 11:08:27 +0530

    Revert "React Netflix Clone"
    This reverts commit 511f9731fc43c1e3e5e7ba339e3e043e8bce4708.

--- src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import { myAxios } from "./helper";
-
-export const signUp = (user)=>{
-    return myAxios.post("/save", user).then((response)=> response.data)
-};
-
-export const signIn =(user1) =>{
-    return myAxios.post("/login" , user1).then((response)=>response.data)
-}
